212ac06a76c82a856a2d6262cb77c0c1f3dc4036
DJT_Brand <djt.test.server@gmail.com>
2023-08-18 06:19:25 +0000

    Converted from minesweeper example

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,94 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+enum TileState { covered, blown, open, flagged, revealed }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+void main() => runApp(const BoardGameApp());
+
+class BoardGameApp extends StatelessWidget {
+  const BoardGameApp({super.key});
+
+  // This widget is the root of our application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      title: 'Board Game',
+      // The widget which will be started on application startup
+      home: Board(),
+    ); // MaterialApp
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Board extends StatefulWidget {
+  const Board({super.key});
+
+  @override
+  BoardState createState() => BoardState();
+}
+
+class BoardState extends State<Board> {
+  final int rows = 9;
+  final int cols = 9;
+  final int numOfMines = 11; // TODO: Minesweeper specific
+
+  late List<List<TileState>> uiState;
+
+  void resetBoard() {
+    uiState = List<List<TileState>>.generate(rows, (row) {
+      return List<TileState>.filled(cols, TileState.covered);
+    }); // List.generate
+  }
+
+  @override
+  void initState() {
+    resetBoard();
+    super.initState();
+  }
+
+  Widget buildBoard() {
+    List<Row> boardRow = <Row>[];
+    for (int i = 0; i < rows; i++) {
+      List<Widget> rowChildren = <Widget>[];
+      for (int j = 0; j < cols; j++) {
+        TileState state = uiState[i][j];
+        if (state == TileState.covered) {
+          rowChildren.add(GestureDetector(
+            child: Listener(
+              child: Container(
+                margin: const EdgeInsets.all(1.0),
+                height: 20.0,
+                width: 20.0,
+                color: Colors.grey,
+              ),
+            ),
+          ));
+        }
+      }
+      boardRow.add(Row(
+        children: rowChildren,
+        mainAxisAlignment: MainAxisAlignment.center,
+        key: ValueKey<int>(i),
+      ));
+    }
+    return Container(
+      color: Colors.grey[700],
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: boardRow,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: const Text('Board game'),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Container(
+        color: Colors.grey[50],
+        child: Center(
+          child: buildBoard(),
         ),
       ),
     );
